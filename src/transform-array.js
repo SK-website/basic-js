6f21d356a230cda97c7ae87dcad9e5f46bf40954
SK-website <vnali@tutanota.com>
2021-02-20 13:56:22 +0200

    solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+
+  const discardNext = "--discard-next";
+  const discardPrev = "--discard-prev";
+  const doubleNext = "--double-next";
+  const doublePrev = "--double-prev";
+
+  let newArr = arr.concat();
+
+  let res = [];
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] === discardNext) {
+      if (i != newArr.length - 1) newArr[i + 1] = " ";
+    } else if (newArr[i] === discardPrev) {
+      if (i != 0 && !(newArr[i - 1] === " ")) res.pop();
+    } else if (newArr[i] === doubleNext) {
+      if (i != newArr.length - 1) res.push(newArr[i + 1]);
+    } else if (newArr[i] === doublePrev) {
+      if (i != 0 && !(newArr[i - 1] === " ")) res.push(newArr[i - 1]);
+    } else if (!(newArr[i] === " ")) res.push(newArr[i]);
+  }
+  console.log(res);
+
+  return res;
 };
