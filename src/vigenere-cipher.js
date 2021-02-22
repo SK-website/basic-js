cde68914106f4a97ec09dfe520b5ff91e4084b51
SK-website <vnali@tutanota.com>
2021-02-22 19:10:11 +0200

    soluyion w/a 1 test

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,100 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(string, key, direct) {
+    this.string = string;
+    this.key = key;
+    this.direct = direct;
+    this.letters = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+  }
+
+  getLetterIndex(letter) {
+    for (let i = 0; i < this.letters.length; i++) {
+      if (letter === this.letters[i]) return i;
+    }
+    return -1;
+  }
+
+  encrypt(string, key) {
+    if (string === undefined || key === undefined)
+      throw new Error(`Please give arguments`);
+    let keyArr = key.toUpperCase().split("");
+    let strArr = string.toUpperCase().split("");
+    let resArr = [];
+    let n = -1;
+
+    for (let i = 0; i < strArr.length; i++) {
+      let currStrIndx = this.getLetterIndex(strArr[i]);
+      if (currStrIndx < 0) {
+        resArr.push(strArr[i]);
+        continue;
+      }
+      n = (n + 1) % keyArr.length;
+      let currKeyIndx = this.getLetterIndex(keyArr[n]);
+
+      let nextLetterIndx = (currStrIndx + currKeyIndx) % this.letters.length;
+      resArr.push(this.letters[nextLetterIndx]);
+    }
+    if (this.direct === false) {
+      resArr = resArr.reverse();
+      return resArr.join("");
+    }
+    return resArr.join("");
+  }
+
+  decrypt(string, key) {
+    if (string === undefined || key === undefined)
+      throw new Error(`Please give arguments`);
+    let keyArr = key.toUpperCase().split("");
+    let strArr = string.toUpperCase().split("");
+
+    let resArr = [];
+    let n = -1;
+    for (let i = 0; i < strArr.length; i++) {
+      let currStrIndx = this.getLetterIndex(strArr[i]);
+      if (currStrIndx < 0) {
+        resArr.push(strArr[i]);
+        continue;
+      }
+      n = (n + 1) % keyArr.length;
+      let currKeyIndx = this.getLetterIndex(keyArr[n]);
+
+      let nextLetterIndx =
+        (currStrIndx - currKeyIndx + 26) % this.letters.length;
+      resArr.push(this.letters[nextLetterIndx]);
+    }
+    if (this.direct === false) {
+      resArr = resArr.reverse();
+      return reaArr.join("");
+    }
+    return resArr.join("");
   }
 }
 
